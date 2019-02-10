c9f93f9f20d20fe0b18c3060cb331679840576cf
Fred Duby <fcd1@columbia.edu>
2019-02-09 22:53:48 -0500

    End Sec 4, Lec 57: Flexible Lists

--- src/App.js
+++ src/App.js
@@ -5,35 +5,54 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      {name: 'Max', age: 28},
-      {name: 'Manu', age: 29},
-      {name: 'Stephanie', age: 26}
+      { id: 1, name: 'Max', age: 28},
+      { id: 2, name: 'Manu', age: 29},
+      { id: 3, name: 'Stephanie', age: 26}
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('Was Clicked');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maxi';
-    this.setState({
-      persons: [
-        {name: newName, age: 28},
-        {name: 'Manu', age: 29},
-        {name: 'Stephanie', age: 27}
-      ]
-    });
-  }
-
   // event arg will be passed in automatically by React
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        {name: 'Max', age: 28},
-        {name: event.target.value, age: 29},
-        {name: 'Stephanie', age: 27}
-      ]
-    });
+  nameChangeHandler =
+    (event, id) =>
+    {
+      const personIndex =
+        this.state.persons.findIndex(p => {
+          return p.id === id;
+          }
+        );
+
+      // Make a copy of the person object
+      const person = {
+        ...this.state.persons[personIndex]
+      };
+
+      person.name = event.target.value;
+
+      const persons = [...this.state.persons];
+      persons[personIndex] = person;
+      this.setState( {persons: persons});
+    }
+
+  deletePersonHandler = (personIndex) => {
+    // NOTE: Old code had the following:
+    // const persons = this.state.persons
+    // However, that means that, when we do the splice,
+    // we will be splicing the array directly within the state,
+    // which can lead to weirdness. Instead, make a copy of it and
+    // change the copy. See Sec 4, Lec 55
+    // Always update state in an immutable fashion:
+    // Make a copy, update that, then use setState
+
+    // Calling slice without any arguments
+    // is a way to copy the array.
+    // const persons = this.state.persons.slice();
+    // Can also use the spread operator
+    // this is the more modern way to do it
+    const persons = [...this.state.persons];
+    persons.splice(personIndex,1);
+    this.setState({persons: persons});
   }
 
   togglePersonsHandler = () => {
@@ -55,27 +74,20 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          >
-            <em>Hello</em>
-          </Person>
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            // Other way to pass argument(s)
-            click={this.switchNameHandler.bind(this,'MaxiParagraph')}
-            // event arg passed in automatically by React
-            changed={this.nameChangeHandler}
-          >
-            Hobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          >
-          </Person>
+          {this.state.persons.map(
+            (person, index) => {
+              return <Person
+                        click={() => this.deletePersonHandler(index)}
+                        name={person.name}
+                        age={person.age}
+                        // key helps react figure out which element is a list needs to be updated when refeshing DOM
+                        // will get rid of following warning we have been getting:
+                        // Warning: Each child in a list should have a unique "key" prop.
+                        key={person.id}
+                        changed={(event) => this.nameChangeHandler(event, person.id)}
+                      />
+            }
+          )}
         </div>
       )
     }
@@ -83,17 +95,11 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        { 
-          /* One way to pass argumentss()
-          However, Max said this can be ineffecient: Re-render things too often.
-          He recomments the other way (bind syntax shown below)
-          */
-        }
         <button
           onClick={this.togglePersonsHandler}
           style={style}
         >
-          Show Names
+          Toggle Persons
         </button>
         {persons}
       </div>
