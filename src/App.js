17ab1d0ca273d709e93ec8049b595d936f3e4d61
Fred Duby <fcd1@columbia.edu>
2019-02-12 14:28:23 -0500

    End Sec 5, Lec 65: Using Radium for Media Queries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -67,7 +68,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     // Preferred way of outputing content conditionaly
@@ -93,6 +98,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmom',
+        color: 'black'
+      }
     }
 
     // let classes = ['red', 'bold'].join(' ');
@@ -106,19 +115,21 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          onClick={this.togglePersonsHandler}
-          style={style}
-        >
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
+      <StyleRoot> 
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button
+            onClick={this.togglePersonsHandler}
+            style={style}
+          >
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
