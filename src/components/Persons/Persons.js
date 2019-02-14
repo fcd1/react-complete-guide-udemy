7b19e46b276aa8d09ff4a6dd1d55b15393bccd60
Fred Duby <fcd1@columbia.edu>
2019-02-14 15:14:21 -0500

    Comment out code as done in Section 7, Lecture 86

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,10 +2,10 @@ import React, { PureComponent } from 'react';
 import Person from './Person/Person';
 
 class Persons extends PureComponent {
-    static getDerivedStateFromProps(props, state) {
-        console.log('[Persons.js] getDreviedStateFromProps');
-        return state;
-    }
+    // static getDerivedStateFromProps(props, state) {
+    //     console.log('[Persons.js] getDreviedStateFromProps');
+    //     return state;
+    // }
 
     // shouldComponentUpdate(nextProps, nextState) {
     //     console.log('[Persons.js] shouldComponentUpdate');
