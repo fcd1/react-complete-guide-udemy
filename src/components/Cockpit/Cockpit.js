feea9c8b2c78c88b838fec5a8cceed8b61957066
Fred Duby <fcd1@columbia.edu>
2019-02-14 10:29:01 -0500

    End Sec 7, Lec 91: Cleanup Work with useEffects - Ex)

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Cockpit.module.css';
 
 const cockpit = (props) => {
+    useEffect(
+        () => {
+            console.log('[Cockpit.js] useEffect');
+            // Mimic http request
+            const timer = setTimeout(
+                () => {
+                    alert('Saved data to cloud!');
+                },
+                2000
+            );
+            return () => {
+                clearTimeout(timer);
+                console.log('[Cockpit.js] cleanup work in useEffect');
+            };
+        },
+        // If pass an empty array (i.e should run when a depency changes, but has no depencies),
+        // it will run the initial first time, but then never again
+        [props.persons]
+    );
+
+    useEffect(
+        () => {
+            console.log('[Cockpit.js] 2nd useEffect');
+            return () => {
+                console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+            }
+        }
+    );
+
     const classes = [];
     let btnClass = '';
     if (props.showPersons) {
